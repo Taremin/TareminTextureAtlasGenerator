10a710770e7fd3da75a725358665f0adafd6f3e1
Taremin <taremin.vr@gmail.com>
2021-01-05 17:23:23 +0900

    release 0.0.2

--- __init__.py
+++ __init__.py
@@ -35,7 +35,7 @@ bl_info = {
     'author': 'Taremin',
     'location': 'View 3D > Tools',
     'description': "Generate texture atlas from selected objects",
-    'version': (0, 0, 1),
+    'version': (0, 0, 2),
     'blender': (2, 80, 0),
     'wiki_url': '',
     'tracker_url': '',
